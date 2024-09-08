710f944d6fa39f549a1b64c37ddb6b7c2402bda7
SLMCreatives <sulaiman@slmcreatives.com>
2024-09-08 15:57:25 +0800

    login logic out

--- pakar/src/app/login/page.tsx
+++ pakar/src/app/login/page.tsx
@@ -1,29 +1,11 @@
 "use client";
 
-import { login, signup } from "./action";
 import Loginpage from "@/components/authentication-04";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "../../../node_modules/next/navigation";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { revalidatePath } from "../../../node_modules/next/cache";
-import { signout } from "../signout/action";
 
 export default async function LoginPage() {
-  const supabase = createClient();
-  const { data: user } = await supabase.auth.getUser();
-  if (user.user !== null) {
-    return (
-      <div className="flex flex-col gap-2 h-screen items-center justify-center text-white">
-        <p>Already logged in as {user.user.email}</p>
-        <Link href="/dashboard">Go To Dashboard</Link>
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex h-screen items-center justify-center text-white">
-        <Loginpage />
-      </div>
-    );
-  }
+  return (
+    <div className="flex h-screen items-center justify-center text-white">
+      <Loginpage />
+    </div>
+  );
 }
